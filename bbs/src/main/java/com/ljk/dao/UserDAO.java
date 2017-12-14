03714de1d11b37f99444b43c937df259abb1f310
linjiekuan <597690851@qq.com>
2017-12-14 10:56:27 +0800

    写好了userDAO，是关于用户登录的DAO，并且映射了对应的XMl

---
+++ bbs/src/main/java/com/ljk/dao/UserDAO.java
@@ -0,0 +1,14 @@
+package com.ljk.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+import com.ljk.po.User;
+@Repository //spring的注解 ，用于标注数据访问组件,即DAO组件，把这些类纳入进spring容器中管理
+public interface UserDAO {
+	//登陆验证
+	public User userLogin(@Param("username") String username, 
+			@Param("password") String password); 
+	
+}
