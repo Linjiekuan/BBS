6c3411e77be243a22bb5e06a1ce6250382c0c9d4
linjiekuan <597690851@qq.com>
2017-12-14 10:41:38 +0800

    配置了spring，web.xml，整合了一下ssm框架

---
+++ bbs/src/main/java/com/ljk/po/User.java
@@ -0,0 +1,82 @@
+package com.ljk.po;
+
+public class User {
+	private int id;
+	private String username;
+	private String password;
+	private String email;
+	private String number;
+	private String ext01;
+	private String ext02;
+	private String ext03;
+	private String ext04;
+	private String ext05;
+	private String ext06;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getExt01() {
+		return ext01;
+	}
+	public void setExt01(String ext01) {
+		this.ext01 = ext01;
+	}
+	public String getExt02() {
+		return ext02;
+	}
+	public void setExt02(String ext02) {
+		this.ext02 = ext02;
+	}
+	public String getExt03() {
+		return ext03;
+	}
+	public void setExt03(String ext03) {
+		this.ext03 = ext03;
+	}
+	public String getExt04() {
+		return ext04;
+	}
+	public void setExt04(String ext04) {
+		this.ext04 = ext04;
+	}
+	public String getExt05() {
+		return ext05;
+	}
+	public void setExt05(String ext05) {
+		this.ext05 = ext05;
+	}
+	public String getExt06() {
+		return ext06;
+	}
+	public void setExt06(String ext06) {
+		this.ext06 = ext06;
+	}
+
+}
